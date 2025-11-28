05964246773cfdd0302e3f96ed5955f5e66083d0
kamrulmiazi <kamrul116522@gmail.com>
2025-11-28 16:14:41 +0600

    update url

--- hook/axiosPublic.js
+++ hook/axiosPublic.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosPublic = axios.create({
-  baseURL: "https://blogify-backend-ashen.vercel.app",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
